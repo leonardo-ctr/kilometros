ee5ca798c11fa79c83e19341267e046027d75f2e
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-23 20:43:13 -0600

    Add files via upload

---
+++ kilometros.py
@@ -0,0 +1,10 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, gasto de combustible")
+#solicita los valores
+kilometros=float(input("Ingresa la cantidad de kilómetros recorridos:"))
+combustible=float(input("Ingresa la cantidad de litros usado: "))
+
+print(" ")#define una linea en blanco
+consumo = combustible / kilometros#la operacion que se realizara
+print(" ")#define una linea en blanco
+print("El consumo de combustible es de", consumo)#muestra el resultado
